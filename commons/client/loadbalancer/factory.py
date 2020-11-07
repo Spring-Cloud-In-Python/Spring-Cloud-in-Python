800cb38fd3b81f199f851219e5c971d15daf5201
johnny850807 <johnny850807@gmail.com>
2020-11-07 13:24:57 +0800

    :bug: fix cyclic dependency

--- commons/client/loadbalancer/factory.py
+++ commons/client/loadbalancer/factory.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 # scip plugin
-from commons.client.loadbalancer.loadbalancer import LoadBalancer
 
 __author__ = "Waterball (johnny850807@gmail.com)"
 __license__ = "Apache 2.0"
 
 
 class LoadBalancerClientFactory:
-    def get_instance(self, service_id: str) -> LoadBalancer:
+    def get_instance(self, service_id: str):
         """
         :param service_id: (str)
-        :return:
+        :return: (LoadBalancer)
         """
         pass
