d494776a420e5428f638af1641631f2231bd700b
SED2020 Group5 <group5_leader_studentid@ntu.edu.tw>
2020-11-16 00:45:23 +0800

    Implement LoadBalancer (interface)

---
+++ ribbon/calculator/calculator.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "MJ (tsngmj@gmail.com)"
+__license__ = "Apache 2.0"
+
+
+class Calculator:
+    def eval(self, expression):
+        return int(expression[0]) + int(expression[4])
