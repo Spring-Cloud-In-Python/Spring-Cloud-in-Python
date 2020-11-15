b31f07a321b4c84a6f6ac7dd32f73a8f28f53fff
Haribo <haribo1558599@gmail.com>
2020-11-15 23:35:28 +0800

    add docstring and test

--- eureka/client/app_info/__init__.py
+++ eureka/client/app_info/__init__.py
@@ -1,2 +1,3 @@
 from .instance_info import InstanceInfo
 from .lease_info import LeaseInfo
+
--- tests/euraka/client/app_info/lease_info_test.py
+++ tests/euraka/client/app_info/lease_info_test.py
@@ -7,6 +7,13 @@ __license__ = "Apache 2.0"
 from eureka.client.app_info.lease_info import LeaseInfo
 
 
-def test_lease_info():
-    lease_info = LeaseInfo(registration_timestamp=10)
-    assert lease_info.registration_timestamp == 10
+def test_lease_info_constructor():
+    lease_info = LeaseInfo(lease_renewal_interval_in_secs=-1, lease_duration_in_secs=-1)
+
+    assert lease_info.lease_renewal_interval_in_secs == lease_info.DEFAULT_LEASE_RENEWAL_INTERVAL
+    assert lease_info.lease_duration_in_secs == lease_info.DEFAULT_LEASE_DURATION
+
+    lease_info = LeaseInfo(lease_renewal_interval_in_secs=1, lease_duration_in_secs=1)
+
+    assert lease_info.lease_renewal_interval_in_secs == 1
+    assert lease_info.lease_duration_in_secs == 1
