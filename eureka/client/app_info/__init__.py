4360e31c7c406cc8d438d37d2bcd20129f3adea2
Haribo <haribo1558599@gmail.com>
2020-11-18 23:57:31 +0800

    revise import in __init__.py

--- eureka/client/app_info/__init__.py
+++ eureka/client/app_info/__init__.py
@@ -1,3 +1,2 @@
 from .instance_info import InstanceInfo
 from .lease_info import LeaseInfo
-
--- eureka/client/discovery/shared/transport/__init__.py
+++ eureka/client/discovery/shared/transport/__init__.py
@@ -1,1 +1,3 @@
 # -*- coding: utf-8 -*-
+
+from .eureka_transport_config import DefaultEurekaTransportConfig, EurekaTransportConfig
