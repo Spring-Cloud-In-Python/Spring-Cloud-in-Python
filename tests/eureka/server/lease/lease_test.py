80c06f0a10d63ad2e875c762f329d206a6afa5f2
Daniel1147 <sxn91401@gmail.com>
2020-11-19 14:13:09 +0800

    arrange tests into a class

--- tests/eureka/server/lease/lease_test.py
+++ tests/eureka/server/lease/lease_test.py
@@ -11,10 +11,6 @@ from eureka.server.lease.lease import Lease
 from eureka.utils.timestamp import current_timestamp
 
 
-class FakeLeaseInfo:
-    pass
-
-
 def equal_with_tolerance(expected, actual):
     tolerance = 50  # tolerant small measurement error
     diff = abs(actual - expected)
@@ -22,99 +18,97 @@ def equal_with_tolerance(expected, actual):
     return diff < tolerance
 
 
-def test_initial_lease_not_expired():
-    fake_lease_info = FakeLeaseInfo()
-    lease = Lease(fake_lease_info, 0)
+class TestLease:
+    class FakeLeaseInfo:
+        pass
 
-    assert not lease.is_expired()
+    def test_initial_lease_not_expired(self):
+        fake_lease_info = self.FakeLeaseInfo()
+        lease = Lease(fake_lease_info, 0)
 
+        assert not lease.is_expired()
 
-def test_setters():
-    fake_lease_info = FakeLeaseInfo()
-    fake_lease_info2 = FakeLeaseInfo()
+    def test_setters(self):
+        fake_lease_info = self.FakeLeaseInfo()
+        fake_lease_info2 = self.FakeLeaseInfo()
 
-    lease = Lease(fake_lease_info, 0)
-    lease.service_up_timestamp = 123
+        lease = Lease(fake_lease_info, 0)
+        lease.service_up_timestamp = 123
 
-    assert lease.holder != fake_lease_info2
-    assert lease.service_up_timestamp == 123
+        assert lease.holder != fake_lease_info2
+        assert lease.service_up_timestamp == 123
 
-    lease = Lease(fake_lease_info2, 0)
+        lease = Lease(fake_lease_info2, 0)
 
-    assert lease.holder == fake_lease_info2
+        assert lease.holder == fake_lease_info2
 
+    def test_current_timestamps_should_be_logical(self):
+        start = current_timestamp()
 
-def test_current_timestamps_should_be_logical():
-    start = current_timestamp()
+        fake_lease_info = self.FakeLeaseInfo()
+        lease = Lease(fake_lease_info, 0)
 
-    fake_lease_info = FakeLeaseInfo()
-    lease = Lease(fake_lease_info, 0)
+        assert lease.registration_timestamp >= start <= current_timestamp()
+        assert equal_with_tolerance(start, lease.registration_timestamp)
+        assert equal_with_tolerance(start, lease.last_update_timestamp)
 
-    assert lease.registration_timestamp >= start <= current_timestamp()
-    assert equal_with_tolerance(start, lease.registration_timestamp)
-    assert equal_with_tolerance(start, lease.last_update_timestamp)
+    def test_renew(self):
+        start = current_timestamp()
 
+        fake_lease_info = self.FakeLeaseInfo()
+        lease = Lease(fake_lease_info, 0)
 
-def test_renew():
-    start = current_timestamp()
+        time_passed = 0.1  # pass 0.1 second
+        time.sleep(time_passed)
+        current = start + time_passed * 1000
+        last_update = current
+        lease.renew()
 
-    fake_lease_info = FakeLeaseInfo()
-    lease = Lease(fake_lease_info, 0)
+        assert equal_with_tolerance(start, lease.registration_timestamp)
+        assert equal_with_tolerance(last_update, lease.last_update_timestamp)
 
-    time_passed = 0.1  # pass 0.1 second
-    time.sleep(time_passed)
-    current = start + time_passed * 1000
-    last_update = current
-    lease.renew()
+    def test_cancel(self):
+        start = current_timestamp()
 
-    assert equal_with_tolerance(start, lease.registration_timestamp)
-    assert equal_with_tolerance(last_update, lease.last_update_timestamp)
+        fake_lease_info = self.FakeLeaseInfo()
+        lease = Lease(fake_lease_info, 0)
 
+        time_passed = 0.1  # pass 0.1 second
+        time.sleep(time_passed)
+        current = start + time_passed * 1000
+        cancel_time = current
+        lease.cancel()
 
-def test_cancel():
-    start = current_timestamp()
+        assert equal_with_tolerance(start, lease.registration_timestamp)
+        assert equal_with_tolerance(cancel_time, lease.eviction_timestamp)
 
-    fake_lease_info = FakeLeaseInfo()
-    lease = Lease(fake_lease_info, 0)
+    def test_service_up(self):
+        start = current_timestamp()
 
-    time_passed = 0.1  # pass 0.1 second
-    time.sleep(time_passed)
-    current = start + time_passed * 1000
-    cancel_time = current
-    lease.cancel()
+        fake_lease_info = self.FakeLeaseInfo()
+        lease = Lease(fake_lease_info, 0)
 
-    assert equal_with_tolerance(start, lease.registration_timestamp)
-    assert equal_with_tolerance(cancel_time, lease.eviction_timestamp)
+        time_passed = 0.1  # pass 0.1 second
+        time.sleep(time_passed)
+        current = start + time_passed * 1000
+        service_uptime = current
+        lease.service_up()
 
+        assert equal_with_tolerance(start, lease.registration_timestamp)
+        assert equal_with_tolerance(service_uptime, lease.service_up_timestamp)
 
-def test_service_up():
-    start = current_timestamp()
+    def test_is_expired(self):
+        lease_expire_time_in_secs = 1
 
-    fake_lease_info = FakeLeaseInfo()
-    lease = Lease(fake_lease_info, 0)
+        fake_lease_info = self.FakeLeaseInfo()
+        lease = Lease(fake_lease_info, lease_expire_time_in_secs)
 
-    time_passed = 0.1  # pass 0.1 second
-    time.sleep(time_passed)
-    current = start + time_passed * 1000
-    service_uptime = current
-    lease.service_up()
+        assert not lease.is_expired()
 
-    assert equal_with_tolerance(start, lease.registration_timestamp)
-    assert equal_with_tolerance(service_uptime, lease.service_up_timestamp)
+        time_passed = 0.7
+        time.sleep(time_passed)
+        assert not lease.is_expired()
 
-
-def test_is_expired():
-    lease_expire_time_in_secs = 1
-
-    fake_lease_info = FakeLeaseInfo()
-    lease = Lease(fake_lease_info, lease_expire_time_in_secs)
-
-    assert not lease.is_expired()
-
-    time_passed = 0.7
-    time.sleep(time_passed)
-    assert not lease.is_expired()
-
-    time_passed = 0.7
-    time.sleep(time_passed)
-    assert lease.is_expired()
+        time_passed = 0.7
+        time.sleep(time_passed)
+        assert lease.is_expired()
