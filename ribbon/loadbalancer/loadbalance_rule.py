e804c72e11213850b16b6286ceacff5e0979e26b
SED2020 Group5 <group5_leader_studentid@ntu.edu.tw>
2020-11-21 00:41:11 +0800

    fIx LoadBalancerRule

--- ribbon/loadbalancer/loadbalance_rule.py
+++ ribbon/loadbalancer/loadbalance_rule.py
@@ -2,3 +2,30 @@
 
 __author__ = "MJ (tsngmj@gmail.com)"
 __license__ = "Apache 2.0"
+
+# standard library
+from abc import ABC, abstractmethod
+
+# scip plugin
+from ribbon.loadbalancer.loadbalancer import LoadBalancer
+from ribbon.loadbalancer.server import Server
+
+"""
+Interface that defines a "Rule" for a LoadBalancer. A Rule can be thought of
+as a Strategy for loadbalacing. Well known loadbalancing strategies include
+Round Robin, Response Time based etc.
+"""
+
+
+class LoadBalanceRule(ABC):
+    @abstractmethod
+    def choose(self, key: object) -> Server:
+        pass
+
+    @abstractmethod
+    def set_loadbalancer(self, lb: LoadBalancer):
+        pass
+
+    @abstractmethod
+    def get_loadbalancer(self):
+        pass
