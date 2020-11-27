86235fef78f58e3e495673265d1c40997539e0ed
Daniel1147 <sxn91401@gmail.com>
2020-11-27 10:37:08 +0800

    rename to RegistryPresenter

--- eureka/server/registry/instance_registry.py
+++ eureka/server/registry/instance_registry.py
@@ -12,7 +12,7 @@ import threading
 from eureka.client.app_info import InstanceInfo, LeaseInfo
 from eureka.client.discovery.shared import Application, Applications
 from eureka.server.lease.lease import Lease
-from eureka.server.registry.registry_responser import RegistryResponser
+from eureka.server.registry.registry_presenter import RegistryPresenter
 from eureka.utils.concurrent import ConcurrentMap
 
 
@@ -53,7 +53,7 @@ class InstanceRegistry:
     def __init__(self):
         self.registry = ConcurrentMap()
         self.lock = threading.RLock()
-        self.responser = RegistryResponser(self)
+        self.presenter = RegistryPresenter(self)
 
     def register(self, registrant: InstanceInfo, lease_duration: int):
         """
@@ -140,5 +140,5 @@ class InstanceRegistry:
             applications.reconciliation_hash_code = applications.compute_reconciliation_hash_code()
             return applications
 
-    def get_responser(self) -> RegistryResponser:
-        return self.responser
+    def get_responser(self) -> RegistryPresenter:
+        return self.presenter
