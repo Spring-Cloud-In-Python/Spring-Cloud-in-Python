64d94da2d57c44313d6aa4d2720fa7aac98f2c37
yanci <ck6990ck6990@gmail.com>
2020-11-29 21:19:25 +0800

    Dev/37 Spring-Cloud-Netflix EurekaDiscoveryClient implement (#68)
    * eureka service instance implement practice
    
    * v1
    
    * modified
    
    * add eureka_discovery_client.py
    
    * modified
    
    * can be ignore
    
    * can be ignore
    
    * intergration with instance_info of eureka-client
    
    * delete a uncorrelated print command
    
    * Revert "delete a uncorrelated print command"
    
    This reverts commit d414afeec06347ea4cf8b6f134e4fd450f270a29.
    
    * Revert "Revert "delete a uncorrelated print command""
    
    This reverts commit 0f3d939d27c42fc95335438459f229775ccb32ce.
    
    * modify to property
    
    * modify list comprehension
    
    * modify list comprehension
    
    Co-authored-by: Yan Ci Su <yanci@Yande-MacBook-Pro-2.local>

---
+++ spring_cloud/eureka/eureka_discovery_client.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Ricky"
+__license__ = "Apache 2.0"
+
+# standard library
+from typing import List
+
+# scip plugin
+from spring_cloud.commons.client.discovery.discovery_client import DiscoveryClient
+from spring_cloud.commons.client.service_instance import ServiceInstance
+from spring_cloud.eureka.eureka_service_instance import EurekaServiceInstance
+
+
+class EurekaDiscoveryClient(DiscoveryClient):
+    def __init__(self, eureka_client, client_config):
+        self.DESCRIPTION = "Spring Cloud Eureka Discovery Client"
+        self.eureka_client = eureka_client
+        self.client_config = client_config
+
+    def get_instances(self, service_id: str) -> List[ServiceInstance]:
+        instance_info = self.eureka_client.get_instances_by_vip_address(service_id, False)
+        instances = [EurekaServiceInstance(x) for x in instance_info]
+        return instances
+
+    @property
+    def services(self) -> List[str]:
+        applications = self.eureka_client.get_applications()
+        if not applications:
+            return []
+        registered_apps = applications.get_registered_applications()
+        return [app.get_name().lower() for app in registered_apps if not app.get_instances().is_empty()]
