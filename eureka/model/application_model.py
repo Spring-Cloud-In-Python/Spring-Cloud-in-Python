46a8bf05f208a15d5fe182516208be4b490e0429
Daniel1147 <sxn91401@gmail.com>
2020-12-01 18:02:47 +0800

    Add model ApplicationModel and ApplicationsModel

--- eureka/model/application_model.py
+++ eureka/model/application_model.py
@@ -4,32 +4,37 @@ from __future__ import annotations
 __author__ = "Daniel1147 (sxn91401@gmail.com)"
 __license__ = "Apache 2.0"
 
+# standard library
+from typing import List
+
 # pypi/conda library
 from pydantic import BaseModel
 
 # scip plugin
 from eureka.client.app_info import LeaseInfo
+from eureka.client.discovery.shared import Application
+from eureka.model.instance_info_model import InstanceInfoModel
 
 
-class LeaseInfoModel(BaseModel):
-    registration_timestamp: int
-    last_renewal_timestamp: int
-    eviction_timestamp: int
-    service_up_timestamp: int
-    lease_renewal_interval_in_secs: int
-    lease_duration_in_secs: int
+class ApplicationModel(BaseModel):
+    name: str
+    instance_info_model_list: List[InstanceInfoModel]
 
     @staticmethod
-    def from_entity(lease_info: LeaseInfo) -> LeaseInfoModel:
-        obj = {}
-        for property_name in LeaseInfo.__slots__:
-            property_key = property_name[1:]  # skip the beginning "_"
+    def from_entity(application: Application) -> ApplicationModel:
+        obj = {"name": application.name}
+        instance_info_list = application.get_all_instances_from_local_cache()
+        instance_info_model_list = []
+        for instance in instance_info_list:
+            instance_info_model_list.append(InstanceInfoModel.from_entity(instance))
+        obj["instance_info_model_list"] = instance_info_model_list
 
-            obj[property_key] = getattr(lease_info, property_name)
-
-        return LeaseInfoModel(**obj)
+        return ApplicationModel(**obj)
 
     def to_entity(self) -> LeaseInfo:
-        lease_info = LeaseInfo(**self.dict())
+        application = Application(self.name)
+        for instance_info_model in self.instance_info_model_list:
+            instance = instance_info_model.to_entity()
+            application.add_instance(instance)
 
-        return lease_info
+        return application
