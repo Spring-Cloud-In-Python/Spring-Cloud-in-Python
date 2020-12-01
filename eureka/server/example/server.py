c4641588c1bac3759eb477590a48f564521f0a0e
Daniel1147 <sxn91401@gmail.com>
2020-12-02 00:35:26 +0800

    Add the eureka server.

---
+++ eureka/server/example/server.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Daniel1147 (sxn91401@gmail.com)"
+__license__ = "Apache 2.0"
+
+# pypi/conda library
+import uvicorn
+from fastapi import FastAPI
+
+# scip plugin
+from eureka.model.application_model import ApplicationModel
+from eureka.model.applications_model import ApplicationsModel
+from eureka.model.instance_info_model import InstanceInfoModel
+from eureka.server.registry.instance_registry import InstanceRegistry
+
+DEFAULT_DURATION = 30
+
+eureka_server = FastAPI()
+registry = InstanceRegistry()
+
+
+@eureka_server.post("/eureka/v2/apps/{app_id}")
+def register_instance(request: InstanceInfoModel, app_id: str):
+    instance_info = request.to_entity()
+    instance_info.app_name = app_id
+
+    registry.register(instance_info, DEFAULT_DURATION)
+
+    return {}
+
+
+@eureka_server.get("/eureka/v2/apps/{app_id}")
+def get_application(app_id: str) -> ApplicationModel:
+    return registry.get_presenter().query_application(app_id)
+
+
+@eureka_server.get("/eureka/v2/apps")
+def get_applications() -> ApplicationsModel:
+    return registry.get_presenter().query_applications()
+
+
+if __name__ == "__main__":
+    # standard library
+    import os
+
+    port = int(os.getenv("port"))
+    uvicorn.run(eureka_server, host="0.0.0.0", port=port)
