2651b8eb447845fe69536f90c01d0f32fb8db421
SED2020 Group5 <group5_leader_studentid@ntu.edu.tw>
2020-12-02 21:04:52 +0800

    Tmp

---
+++ ribbon/eureka/discovery_enabled_server.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "MJ (tsngmj@gmail.com)"
+__license__ = "Apache 2.0"
+
+# scip plugin
+from ribbon.loadbalancer.server import Server
+
+
+class DiscoveryEnabledServer(Server):
+
+    pass
