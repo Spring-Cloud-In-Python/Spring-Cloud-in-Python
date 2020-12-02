f009038787eba7f480184baf7c0fb59d0049365e
Daniel1147 <sxn91401@gmail.com>
2020-12-02 23:56:25 +0800

    Construct TestConcurrentCircularList

--- tests/eureka/utils/concurrent_test.py
+++ tests/eureka/utils/concurrent_test.py
@@ -7,31 +7,30 @@ __license__ = "Apache 2.0"
 from eureka.utils.concurrent import ConcurrentCircularList
 
 
-def test_append_to_concurrent_circular_list():
-    concurrent_circular_list = ConcurrentCircularList()
-    concurrent_circular_list.append(1)
-    concurrent_circular_list.append(2)
-    concurrent_circular_list.append(3)
+class TestConcurrentCircularList:
+    def test_append_to_concurrent_circular_list(self):
+        concurrent_circular_list = ConcurrentCircularList()
+        concurrent_circular_list.append(1)
+        concurrent_circular_list.append(2)
+        concurrent_circular_list.append(3)
 
-    assert concurrent_circular_list._circular_list == [1, 2, 3]
+        assert concurrent_circular_list._circular_list == [1, 2, 3]
 
+    def test_iterate_concurrent_circular_list(self):
+        concurrent_circular_list = ConcurrentCircularList([1, 2, 3])
 
-def test_iterate_concurrent_circular_list():
-    concurrent_circular_list = ConcurrentCircularList([1, 2, 3])
+        assert next(concurrent_circular_list) == 1
+        assert next(concurrent_circular_list) == 2
+        assert next(concurrent_circular_list) == 3
+        assert next(concurrent_circular_list) == 1
+        assert next(concurrent_circular_list) == 2
+        assert next(concurrent_circular_list) == 3
 
-    assert next(concurrent_circular_list) == 1
-    assert next(concurrent_circular_list) == 2
-    assert next(concurrent_circular_list) == 3
-    assert next(concurrent_circular_list) == 1
-    assert next(concurrent_circular_list) == 2
-    assert next(concurrent_circular_list) == 3
-
-
-def test_iterate_empty_concurrent_circular_list():
-    concurrent_circular_list = ConcurrentCircularList()
-    try:
-        next(concurrent_circular_list)
-    except StopIteration:
-        assert True
-    else:
-        assert False
+    def test_iterate_empty_concurrent_circular_list(self):
+        concurrent_circular_list = ConcurrentCircularList()
+        try:
+            next(concurrent_circular_list)
+        except StopIteration:
+            assert True
+        else:
+            assert False
