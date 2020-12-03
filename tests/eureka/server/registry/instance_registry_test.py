7e34fc5308ae2cc9851296e54f53e62777019bc0
Daniel1147 <sxn91401@gmail.com>
2020-12-03 15:07:05 +0800

    Use instance info stub in the instance registry test.

--- tests/eureka/server/registry/instance_registry_test.py
+++ tests/eureka/server/registry/instance_registry_test.py
@@ -4,46 +4,16 @@ __author__ = "Daniel1147 (sxn91401@gmail.com)"
 __license__ = "Apache 2.0"
 
 # scip plugin
-from eureka.client.app_info import InstanceInfo, LeaseInfo
 from eureka.server.registry.instance_registry import InstanceRegistry
+from tests.eureka.client.discovery.shared.stubs import instance_info
 
 
 class TestInstanceRegistry:
     def setup_method(self):
         self.instance_info_list = [
-            InstanceInfo(
-                instance_id="instance_1",
-                app_name="app_name",
-                app_group_name="app_group_name",
-                ip_address="127.0.0.1",
-                vip_address="stub-service",
-                secure_vip_address="stub-service",
-                lease_info=LeaseInfo(),
-                metadata={},
-                host_name="localhost",
-            ),
-            InstanceInfo(
-                instance_id="instance_2",
-                app_name="app_name",
-                app_group_name="app_group_name",
-                ip_address="127.0.0.1",
-                vip_address="stub-service",
-                secure_vip_address="stub-service",
-                lease_info=LeaseInfo(),
-                metadata={},
-                host_name="localhost",
-            ),
-            InstanceInfo(
-                instance_id="instance_3",
-                app_name="app_2",
-                app_group_name="app_group_name",
-                ip_address="127.0.0.1",
-                vip_address="stub-service",
-                secure_vip_address="stub-service",
-                lease_info=LeaseInfo(),
-                metadata={},
-                host_name="localhost",
-            ),
+            instance_info(app_name="app_1", num=1),
+            instance_info(app_name="app_1", num=2),
+            instance_info(app_name="app_2", num=3),
         ]
         self.instance_registry = InstanceRegistry()
 
@@ -54,12 +24,12 @@ class TestInstanceRegistry:
 
     def test_get_application(self):
         self.instance_registry.register(self.instance_info_list[0], 10)
-        application = self.instance_registry.get_application("app_name")
-        assert application.name == "app_name"
+        application = self.instance_registry.get_application("app_1")
+        assert application.name == "app_1"
         assert 1 == application.size()
 
         self.instance_registry.register(self.instance_info_list[1], 10)
-        application = self.instance_registry.get_application("app_name")
+        application = self.instance_registry.get_application("app_1")
         assert 2 == application.size()
 
     def test_get_absent_application(self):
@@ -73,8 +43,8 @@ class TestInstanceRegistry:
 
         applications = self.instance_registry.get_applications()
         assert 3 == applications.size()
-        assert 2 == applications.get_registered_application("app_name").size()
+        assert 2 == applications.get_registered_application("app_1").size()
 
         returned_app = applications.get_registered_application("app_2")
         assert 1 == returned_app.size()
-        assert "instance_3" == returned_app.get_instance_by_id("instance_3").instance_id
+        assert "3" == returned_app.get_instance_by_id("3").instance_id
