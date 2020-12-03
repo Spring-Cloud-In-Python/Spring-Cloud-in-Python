1f17aefb2c231b76d1f947d4ff95437d07e24c4b
Chaoyuuu <chaoyu2330@gmail.com>
2020-12-03 21:05:51 +0800

    :sparkles: Add RoutePredicateHandlerMapping & FilteringWebHandler

--- spring_cloud/gateway/server/__init__.py
+++ spring_cloud/gateway/server/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 from .http_request import DefaultServerHttpRequest, ServerHTTPRequest, StaticServerHttpRequest
 from .server import DefaultServerWebExchange, ServerHTTPResponse, ServerWebExchange
+from .utils import GATEWAY_HANDLER_MAPPER_ATTR, GATEWAY_PREDICATE_ROUTE_ATTR, GATEWAY_ROUTE_ATTR
---
+++ tests/gateway/handler/handler_test.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Chaoyuuu (chaoyu2330@gmail.com)"
+__license__ = "Apache 2.0"
+
+
+class TestRoutePredicateHandlerMapping:
+    pass
