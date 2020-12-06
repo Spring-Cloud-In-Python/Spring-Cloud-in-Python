c81415f3351b42bc962ec7d1d6a1f4c915e0b50e
Daniel1147 <sxn91401@gmail.com>
2020-12-06 23:34:34 +0800

    Dev/13: Netflix Eureka Server (Service Registry) Development (#115)
    * Fix import format in tests/eureka/server/registry/instance_registry_test.py.
    
    * Add the eureka server.
    
    * Response with code 204 for successfully registrying.
    
    * Rename the server file.
    
    * Handle invalid requests of registrating an instance with wrong application name.
    
    * API GET /eureka/v2/apps/{app_id} response with 404 when no instances are found.

---
+++ eureka/server/discovery_server.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Daniel1147 (sxn91401@gmail.com)"
+__license__ = "Apache 2.0"
+
+# pypi/conda library
+import uvicorn
+from fastapi import FastAPI, HTTPException, Response, status
+
+# scip plugin
+from eureka.model.application_model import ApplicationModel
+from eureka.model.applications_model import ApplicationsModel
+from eureka.model.instance_info_model import InstanceInfoModel
+from eureka.server.registry.instance_registry import InstanceRegistry
+
+DEFAULT_DURATION = 30
+
+eureka_server = FastAPI()
+registry = InstanceRegistry()
+
+
+@eureka_server.post("/eureka/v2/apps/{app_id}")
+def register_instance(request: InstanceInfoModel, app_id: str):
+    instance_info = request.to_entity()
+
+    if instance_info.app_name != app_id:
+        message = "Application name in the url and the request body should be the same."
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=message)
+
+    registry.register(instance_info, DEFAULT_DURATION)
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@eureka_server.get("/eureka/v2/apps/{app_id}")
+def get_application(app_id: str) -> ApplicationModel:
+    application = registry.get_presenter().query_application(app_id)
+    if application is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+    return registry.get_presenter().query_application(app_id)
+
+
+@eureka_server.get("/eureka/v2/apps")
+def get_applications() -> ApplicationsModel:
+    return registry.get_presenter().query_applications()
+
+
+if __name__ == "__main__":
+    # standard library
+    import os
+
+    port = int(os.getenv("port"))
+    uvicorn.run(eureka_server, host="0.0.0.0", port=port)
