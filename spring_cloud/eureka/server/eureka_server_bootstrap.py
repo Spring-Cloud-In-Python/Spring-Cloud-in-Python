e094fe612a9c9c527362daab82fe4b07aadd5188
Daniel1147 <sxn91401@gmail.com>
2020-12-15 20:07:12 +0800

    EurekaServerBootstrap uses UvicornDiscoveryServer to bootstrap

--- spring_cloud/eureka/server/eureka_server_bootstrap.py
+++ spring_cloud/eureka/server/eureka_server_bootstrap.py
@@ -2,19 +2,24 @@
 
 __author__ = "Ricky"
 __license__ = "Apache 2.0"
+
+# standard library
+from typing import Optional
+
 # pypi/conda library
 import uvicorn
 
 # scip plugin
-from eureka.server.discovery_server import eureka_server, registry
+from eureka.server.discovery_server import UvicornDiscoveryServer
 
 
 class EurekaServerBootstrap:
-    def __int__(self):
-        self._registry = registry
+    def __init__(self):
+        self.__discovery_server = UvicornDiscoveryServer()
 
-    def run(self):
-        uvicorn.run(eureka_server, host="0.0.0.0", port=8787)
+    def run(self, host: Optional[str] = "0.0.0.0", port: Optional[int] = 8787):
+        self.__discovery_server.initialize()
+        self.__discovery_server.run(host=host, port=port)
 
 
 if __name__ == "__main__":
